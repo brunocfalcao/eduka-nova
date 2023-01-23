e6980689f725b883b2f900ceb7d50495c62ce69f
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-01-23 18:16:04 +0100

    wip

---
+++ README.md
@@ -0,0 +1,2 @@
+# eduka-nova
+ Eduka nova
---
+++ src/NovaServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Eduka\Nova;
+
+use Eduka\Abstracts\Classes\EdukaServiceProvider;
+
+class NovaServiceProvider extends EdukaServiceProvider
+{
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    public function register()
+    {
+        //
+    }
+}
